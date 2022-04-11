418b7fadd5d3dd17d3ae3ee6fa957b64defbe05d
Colin Jaworski <91166917+colinjaworski@users.noreply.github.com>
2022-04-11 15:33:06 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,82 +1,57 @@
 
 -- *** CREATE DATABASE NAME: fittruk
 
-
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
-
---- THIS IS THE NEW STUFF BUT IT DEF NEEDS TO BE UPDATED SOMEWHAT ---
-CREATE TABLE "public.users" (
-	"id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"phone number" integer NOT NULL,
-	"address" varchar(255) NOT NULL,
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(255),
+	"email" varchar(255),
+	"phone number" integer,
+	"address" varchar(255),
 	"dob" DATE NOT NULL,
-	"pronouns" varchar(255) NOT NULL,
-	"emergency_name" varchar(255) NOT NULL,
-	"emergency_number" integer NOT NULL,
-	"profile_image" varchar(255) NOT NULL,
-	"free_classes" integer NOT NULL DEFAULT '3',
-	"access_level" integer NOT NULL,
-	CONSTRAINT "users_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+	"pronouns" varchar(255),
+	"emergency_name" varchar(255),
+	"emergency_number" integer,
+	"profile_image" varchar(255),
+	"free_classes" integer DEFAULT '3',
+	"access_level" integer
 );
 
-
-
-CREATE TABLE "public.classes" (
-	"id" serial NOT NULL,
-	"classname" varchar(255) NOT NULL,
-	"description" varchar(500) NOT NULL,
-	"trainer_user_id" integer NOT NULL,
-	"date" DATE NOT NULL,
-	"start_time" TIME NOT NULL,
-	"end_time" TIME NOT NULL,
-	"location" varchar(255) NOT NULL,
-	"class_size" integer NOT NULL,
-	CONSTRAINT "classes_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "classes" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"classname" varchar(255),
+	"description" varchar(500),
+	"trainer_user_id" integer,
+	"date" DATE,
+	"start_time" TIME,
+	"end_time" TIME,
+	"location" varchar(255),
+	"class_size" integer
 );
 
-
-
-CREATE TABLE "public.final attendance" (
-	"id" serial NOT NULL,
-	"class_id" integer NOT NULL,
-	"total_signup" integer NOT NULL,
-	"total_attendance" integer NOT NULL,
-	"total_walkup" integer NOT NULL,
-	CONSTRAINT "final attendance_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "public.class_list" (
-	"id" serial NOT NULL,
-	"class_id" integer NOT NULL,
-	"user_id" integer NOT NULL,
-	"checked_in" BOOLEAN NOT NULL,
-	CONSTRAINT "class_list_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+CREATE TABLE "class_list" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"class_id" int REFERENCES "classes",
+	"user_id" int REFERENCES "users",
+	"checked_in" BOOLEAN DEFAULT false
 );
 
 
 
 
-ALTER TABLE "classes" ADD CONSTRAINT "classes_fk0" FOREIGN KEY ("trainer_user_id") REFERENCES "users"("id");
 
 
-ALTER TABLE "class_list" ADD CONSTRAINT "class_list_fk0" FOREIGN KEY ("class_id") REFERENCES "classes"("id");
-ALTER TABLE "class_list" ADD CONSTRAINT "class_list_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
