c0e43c57ae2217b9639bd72e0f658716b34d5437
Josh Kralewski <joshkmanj@gmail.com>
2022-04-13 10:40:24 -0500

    Class details view added, with basic buttons and click listeners

--- src/components/ClassDetailsPage/ClassDetailsPage.jsx
+++ src/components/ClassDetailsPage/ClassDetailsPage.jsx
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Nav from '../Nav/Nav';
 
 function ClassDetailsPage(){
     const history = useHistory();
@@ -9,10 +10,30 @@ function ClassDetailsPage(){
         history.goBack();
     }
 
+    const gpsHandler = () => {
+        console.log('This would ideally open google maps');
+    }
+
+
+// ----- DELETE this when we have a reducer/saga to replace it ------
+const event = {id: 1,classname: "HIIT",description: "high intensity interval training",trainer_user_id: 2,date: "2022-04-12",start_time: "12:00:00",end_time: "13:00:00",location: "at the park?",class_size: 20};
+// ^^^^^^^^^^^^ DELETE this when we have a reducer/saga to replace it ^^^^^^^^^^^^^^^^^
+
+
+// To be completed on this page:
+// Eventually we'll get a trainer name, that will replace the trainer_user_id below
+// Link the gps to an actual google search query.
 
     return(
         <>
-            <button onClick={handleReturnClick}>return</button>
+            <Nav/>
+            <h1>{event.date}</h1>
+            <h3>{event.classname}</h3>
+            <h3>led by: {event.trainer_user_id}</h3>
+            <h3>{event.location} <button onClick={gpsHandler}>gps</button></h3>
+            <h3>{event.start_time}-{event.end_time}</h3>
+            <h3>{event.description}</h3>
+            <button onClick={handleReturnClick}>Return</button>
         </>
     )
 }
--- src/components/MyClassesPage/MyClassesPage.jsx
+++ src/components/MyClassesPage/MyClassesPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ClassListItem from '../ClassListItem/ClassListItem';
+import Nav from '../Nav/Nav';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -6,11 +8,21 @@ import React from 'react';
 // or even care what the redux state is'
 
 function MyClassesPage() {
+
+
+// ----- DELETE this when we have our classes reducer running------
+const classes = [{id: 1,classname: "HIIT",description: "high intensity interval training",trainer_user_id: 2,date: "2022-04-12",start_time: "12:00:00",end_time: "13:00:00",location: "at the park?",class_size: 20},{id: 3,classname: "Kick boxing",description: "This is a class where we are going to kick and box",trainer_user_id: 1,date: "2022-04-13",start_time: "09:00:00",end_time: "10:00:00",location: "Near the truck",class_size: 18},{id: 2,classname: "Yoga",description: "Its yoga",trainer_user_id: 1,date: "2022-04-13",start_time: "14:00:00",end_time: "15:00:00",location: "Some place noisy",class_size: 10}]
+// ^^^^^^^^^^^^DELETE this when we have our classes reducer running^^^^^^^^^^^^^^^^^
+
+
   return (
-    <div className="container">
-      <div>
-        <p>My Classes page is for registered users to read!</p>
-      </div>
+    <div>
+      <Nav/>
+      <ul>
+      {classes.map((event, i) =>(
+        <ClassListItem event={event} key={i} />
+      ))}
+      </ul>
     </div>
   );
 }
