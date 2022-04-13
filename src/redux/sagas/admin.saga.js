e1cacbe5596d0301887873a94a64efc9c0235dc0
Clay Webb <clay_webb@outlook.com>
2022-04-13 17:14:42 -0500

    Created post action in admin saga for creating a new class

--- src/redux/sagas/admin.saga.js
+++ src/redux/sagas/admin.saga.js
@@ -1,14 +1,20 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
+// ** POST 
+// ** ADMIN ONLY
+// ** Will send a request to the admin router to create a new class and add it to the database
+
 
 // =============***< (POST) CREATE NEW CLASS>***========================
-function* createClass (){
-    
-    // POST 
-    // ADMIN ONLY
-    // will send a request to the admin router to create a new class and add it to the database
-
+function* createClass (action){
+    try {
+        console.log('In createClass saga creating a new class, action.payload is:', action.payload);
+        axios.post('/api/admin', action.payload);
+        yield put({type: 'FETCH_CLASSES'});
+    } catch {
+        console.log('Error trying to create a new class in admin saga!');
+    }    
 }
 
 
