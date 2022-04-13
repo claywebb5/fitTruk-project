ec3c94081565528e19b5ba2f1f33dcba7bc6e523
colin jaworski <colinrjaworski.84@gmail.com>
2022-04-13 15:59:56 -0500

    put request to check in customers for a class in trainer.router

--- server/routes/trainer.router.js
+++ server/routes/trainer.router.js
@@ -11,38 +11,36 @@ const router = express.Router();
 
 
 // -------------------------- check in customers for classes (PUT)
+
 router.put('/:id', (req, res) => {
 
-    console.log('req.body', req.body )
-    console.log('params', req.params.id)
-    const queryText = 
-    `UPDATE "class_list"
+    console.log('req.body.data', req.body.data) // data would be the user_id
+    console.log('params', req.params.id) // params is class id
+
+    if (req.isAuthenticated()) {
+
+        const queryText =
+            `UPDATE "class_list"
     SET "checked_in" = TRUE
     WHERE user_id = $1 and "class_id" = $2`;
 
-    pool.query(queryText, [req.body.data, req.params.id])
+        pool.query(queryText, [req.body.data, req.params.id])
 
-        .then((result) => {
-            res.send(result.rows)
-        })
-        .catch((error) => {
-            console.log(error);
-            res.sendStatus(500);
-        })
+            .then((result) => {
+                res.send(result.rows)
+            })
+            .catch((error) => {
+                console.log(error);
+                res.sendStatus(500);
+            })
 
- 
-
-    // if (req.isAuthenticated()) {
-
-    // } else {
-    //     res.sendStatus(403);
-    // }
+    } else {
+        res.sendStatus(403);
+    }
 
 });
 
-// UPDATE class_list
-// SET checked_in = FALSE
-// WHERE class_id = 1 and user_id = 4;
+
 
 
 
