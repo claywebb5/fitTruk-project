d8a6c94654d43e1e36d5f2d7b2ba80739bf3b714
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-04-13 09:59:04 -0500

    added comments to generator functions

--- src/redux/sagas/class.saga.js
+++ src/redux/sagas/class.saga.js
@@ -2,22 +2,34 @@ import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* fetchClasses (){
+    // GET 
+    // will send a request to the classes router to retrieve all available classes
 
 }
 
 function* searchClasses (){
+    // GET 
+    // will send a request to the classes router to search tne classes by name
 
 }
 
 function* fetchDetails (){
+    // GET
+    // will send a request to the classes router to grab a specific classes details
 
 }
 
 function* updateDetails (){
+    // PUT
+    // TRAINER/ADMIN ONLY
+    // will send a request to the classes router to update the details of a specific class 
 
 }
 
 function* createClasses (){
+    // POST 
+    // ADMIN ONLY
+    // will send a request to the classes router to create a new class and add it to the database
 
 }
 
