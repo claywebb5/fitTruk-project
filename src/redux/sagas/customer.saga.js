82e5f09467997658db267b7e507e78f17121ee60
Clay Webb <clay_webb@outlook.com>
2022-04-13 15:47:45 -0500

    Finished saga actions for getting customer info, and getting all of their registered classes

--- src/redux/sagas/customer.saga.js
+++ src/redux/sagas/customer.saga.js
@@ -2,25 +2,13 @@ import {put, takeLatest, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
 // ** This saga file will listen for the following actions:
-    // *[] Getting all the customers registered classes (My Classes)
     // *[x] Getting all the customers personal info (Personal Info/Profile)
+    // *[] Update customers personal info (Personal Info/Profile)
+    // *[x] Getting all the customers registered classes (My Classes)
     // *[] Adding a new class to their reservations (My/All Classes/Class Details)
     // *[] Removing a class from their reservations (My Classes/Class Details)
 
-// =============***< (GET ALL) REGISTERED CLASSES >***=======================================
-function* fetchCustomerClass() {
-    try {
-        console.log('In fetchCustomerClass, about to axios.get all the customers registered classes');
-        
-        // ** WHERE IN THE ROUTES/SERVER FILES WILL THE ROUTE FOR MY CLASSES BE???
-        const customerClassResponse = yield axios.get('/api/..');
-        
-        console.log('Getting all registered classes:', customerClassResponse.data);
-        yield put({ type: 'SET_MY_CLASSES', payload: customerClassResponse.data });
-    } catch {
-        console.log('Error trying to fetchCustomerClass in sagas!');
-    }
-}
+
 
 // =============***< (GET ALL) CUSTOMER INFO >***=======================================
 function* fetchCustomerInfo(){
@@ -34,7 +22,26 @@ function* fetchCustomerInfo(){
     }
 }
 
-// =============***< (POST) ADD CLASS RESERVATION >***=======================================
+// =============***< (PUT) UPDATE CUSTOMER INFO >***=======================================
+function* updateCustomerInfo(){
+    console.log('In , about to axios.');
+
+
+}
+
+// =============***< (GET ALL) REGISTERED CLASSES >***=======================================
+function* fetchCustomerClass() {
+    try {
+        console.log('In fetchCustomerClass, about to axios.get all the customers registered classes');
+        const customerClassResponse = yield axios.get('/api/customer');
+        console.log('Getting all registered classes:', customerClassResponse.data);
+        yield put({ type: 'SET_MY_CLASSES', payload: customerClassResponse.data });
+    } catch {
+        console.log('Error trying to fetchCustomerClass in sagas!');
+    }
+}
+
+// =============***< (POST) ADD CUSTOMER CLASS RESERVATION >***=======================================
 function* addReservation(action){
     try {
         console.log('The action.payload for adding a new class reservation is:', action.payload)
@@ -48,7 +55,7 @@ function* addReservation(action){
     }    
 }
 
-// =============***< (DELETE) REMOVE CLASS RESERVATION >***=======================================
+// =============***< (DELETE) REMOVE CUSTOMER CLASS RESERVATION >***=======================================
 function* removeReservation(action){
     try {
         console.log('The action.payload in removing a class reservation is:', action.payload)
@@ -74,6 +81,7 @@ function* customerSaga() {
   yield takeLatest('FETCH_CUSTOMER_INFO', fetchCustomerInfo);
   yield takeLatest('ADD_RESERVATION', addReservation);
   yield takeLatest('REMOVE_RESERVATION', removeReservation);
+  yield takeLatest('UPDATE_CUSTOMER_INFO', updateCustomerInfo);
 }
 
 export default customerSaga; // ** NOT EXPORTED INTO ROOT SAGA YET **
