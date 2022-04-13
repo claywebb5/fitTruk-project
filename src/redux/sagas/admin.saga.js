440e4488a5d050e0b08e56d741fa956a84ffcab5
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-04-13 14:56:25 -0500

    created saga for super admin

--- src/redux/sagas/admin.saga.js
+++ src/redux/sagas/admin.saga.js
@@ -11,18 +11,11 @@ function* createClass (){
 
 }
 
-// PUT 
-// SUPER ADMIN ONLY
-// will send a request to the admin router to change a users access level, acting in a way like a promotion
-// =============***< (PUT) UPDATE USER ACCESS LEVEL >***================
-function* updateAccess (){
 
-}
 
 
 function* adminSaga(){
     yield takeLatest('CREATE_CLASS', createClass);
-    yield takeLatest('UPDATE_USER_ACCESS', updateAccess);
 }
 
 export default adminSaga;
