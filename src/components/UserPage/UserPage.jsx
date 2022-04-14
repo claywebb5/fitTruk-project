665d6bd22924e62424c6d22ddc60d18cffa495a4
Clay Webb <clay_webb@outlook.com>
2022-04-14 10:48:38 -0500

    Personal info is appending to the dom

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+// ==================================< PERSONAL INFO PAGE >====================================================
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+  
+  // Default user info from original repo
+  const user = useSelector((store) => store.user); 
+  // Customer info from PersonalinfoReducer (User info ^ is getting all customer info?)
+  const customer = useSelector(store => store.PersonalinfoReducer);
+
+
+  //  ============<ON PAGE LOAD>=============
+  // useEffect(() => {
+  //   dispatch({ type: 'FETCH_CUSTOMER_INFO' }); // Trigger customer.saga
+  // }, []);
+
+
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-    </div>
+    <>
+      <h1><u>Personal Info Page</u></h1>
+      <div className="container">
+        {/* <p>Your ID is: {user.id}</p> */}
+        <h2>Welcome, {user.name}!</h2>
+        <p><b>Pronouns:</b> {user.pronouns}</p>
+        <p><b>Email:</b> {user.email}</p>
+        <p><b>Number:</b> {user.phone_number}</p>
+        <p><b>Address:</b> {user.address}</p>
+        <p>------- In case of emergencies ---------</p>
+        <p><b>Emergency Contact:</b> {user.emergency_name}</p>
+        <p><b>Number:</b> {user.emergency_number}</p>
+
+      </div>
+    </>
   );
 }
 
-// this allows us to use <App /> in index.js
+
 export default UserPage;
