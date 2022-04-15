19f0346aeca9e9b4e0a4ea00a6949d75c1cbdf7c
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-04-15 15:55:56 -0500

    added filter functionailty to MyClassesPage

--- src/components/MyClassesPage/MyClassesPage.jsx
+++ src/components/MyClassesPage/MyClassesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import ClassListItem from '../ClassListItem/ClassListItem';
@@ -19,7 +19,8 @@ function MyClassesPage() {
   const handleAllClassClick = () => {
     history.push("/all-classes");
   }
-
+// ------- Search Bar -------
+const [searchTerm, setSearchTerm] = useState('')
 
 
   const classes = useSelector(store => store.userClass) // SYNTAX-UPDATE: May not be needed, unsure if the 'tested' status mentioned in AllClassesPage
@@ -27,13 +28,23 @@ function MyClassesPage() {
 // const classes = [{id: 1,classname: "HIIT",description: "high intensity interval training",trainer_user_id: 2,date: "2022-04-12",start_time: "12:00:00",end_time: "13:00:00",location: "at the park?",class_size: 20},{id: 3,classname: "Kick boxing",description: "This is a class where we are going to kick and box",trainer_user_id: 1,date: "2022-04-13",start_time: "09:00:00",end_time: "10:00:00",location: "Near the truck",class_size: 18},{id: 2,classname: "Yoga",description: "Its yoga",trainer_user_id: 1,date: "2022-04-13",start_time: "14:00:00",end_time: "15:00:00",location: "Some place noisy",class_size: 10}]
 // ^^^^^^^^^^^^DELETE this when we have our classes reducer running^^^^^^^^^^^^^^^^^
 
-
   return (
     <div>
       <Nav/>
+      <input 
+      type="text"
+      value ={searchTerm}
+      placeholder='Search'
+      onChange={(e) => setSearchTerm(e.target.value)} />
       <button onClick={handleAllClassClick}>All Available classes(this will be an icon eventually)</button>
       <ul>
-      {classes.map((classEvent, i) =>( // "classEvent" was formerly called "event", look out for any residual errors from this.
+      {classes.filter((val)=>{
+        if (searchTerm == ""){
+          return val
+        } else if (val.classname.toLowerCase().includes(searchTerm.toLowerCase())){
+            return val
+        }
+      }).map((classEvent, i) =>( // "classEvent" was formerly called "event", look out for any residual errors from this.
         <ClassListItem classEvent={classEvent} key={i} />
       ))}
       </ul>
