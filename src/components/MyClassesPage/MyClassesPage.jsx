bb9ec7cb7768569e1c9d57edb49ffa16520ca35d
Clay Webb <clay_webb@outlook.com>
2022-04-15 15:38:36 -0500

    Imported useSelector to MyClassesPage

--- src/components/MyClassesPage/MyClassesPage.jsx
+++ src/components/MyClassesPage/MyClassesPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import ClassListItem from '../ClassListItem/ClassListItem';
 import Nav from '../Nav/Nav';
 
