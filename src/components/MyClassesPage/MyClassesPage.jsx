51421d0c63d685c8eeac3125815f8f6ae90a228d
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-04-15 15:44:27 -0500

    this will render the classes a user is signedup for on the dom, page can be refreshed and data remains the accurate

--- src/components/MyClassesPage/MyClassesPage.jsx
+++ src/components/MyClassesPage/MyClassesPage.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import ClassListItem from '../ClassListItem/ClassListItem';
 import Nav from '../Nav/Nav';
 
@@ -7,6 +8,13 @@ import Nav from '../Nav/Nav';
 function MyClassesPage() {
 
   const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_CUSTOMER_CLASS'
+    });
+  }, [])
 
   const handleAllClassClick = () => {
     history.push("/all-classes");
@@ -14,7 +22,7 @@ function MyClassesPage() {
 
 
 
-  const classes = useSelector(store => store.allClasses) // SYNTAX-UPDATE: May not be needed, unsure if the 'tested' status mentioned in AllClassesPage
+  const classes = useSelector(store => store.userClass) // SYNTAX-UPDATE: May not be needed, unsure if the 'tested' status mentioned in AllClassesPage
 // ----- DELETE this when we have our classes reducer running------
 // const classes = [{id: 1,classname: "HIIT",description: "high intensity interval training",trainer_user_id: 2,date: "2022-04-12",start_time: "12:00:00",end_time: "13:00:00",location: "at the park?",class_size: 20},{id: 3,classname: "Kick boxing",description: "This is a class where we are going to kick and box",trainer_user_id: 1,date: "2022-04-13",start_time: "09:00:00",end_time: "10:00:00",location: "Near the truck",class_size: 18},{id: 2,classname: "Yoga",description: "Its yoga",trainer_user_id: 1,date: "2022-04-13",start_time: "14:00:00",end_time: "15:00:00",location: "Some place noisy",class_size: 10}]
 // ^^^^^^^^^^^^DELETE this when we have our classes reducer running^^^^^^^^^^^^^^^^^
