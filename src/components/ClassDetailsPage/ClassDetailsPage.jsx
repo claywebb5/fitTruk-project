47a8308b3a3252f35dc5634442fd1bb34d53c2c3
colin jaworski <colinrjaworski.84@gmail.com>
2022-04-17 10:46:17 -0500

    disables reserve button if class matches reserved class

--- src/components/ClassDetailsPage/ClassDetailsPage.jsx
+++ src/components/ClassDetailsPage/ClassDetailsPage.jsx
@@ -3,8 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Nav from '../Nav/Nav';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
-function ClassDetailsPage(){
+function ClassDetailsPage() {
     //------------<  Setup  >-------------
+    let [match, setMatch] = useState('');
     const history = useHistory();
     const dispatch = useDispatch();
 
@@ -12,17 +13,18 @@ function ClassDetailsPage(){
 
     useEffect(() => {
         dispatch({
-          type: 'FETCH_CLASS_DETAILS',
-          payload: id // PART OF DUMMY DATA, WILL BE UPDATED ONCE :id IS ADDED TO ROUTING
+            type: 'FETCH_CUSTOMER_CLASS',
+            type: 'FETCH_CLASS_DETAILS',
+            payload: id // PART OF DUMMY DATA, WILL BE UPDATED ONCE :id IS ADDED TO ROUTING
         });
-      }, [])
+    }, [])
 
-      const classDetails = useSelector(store => store.selectedClass.classDetails)
-
+    const classDetails = useSelector(store => store.selectedClass.classDetails)
+    const classes = useSelector(store => store.userClass)
 
     //------------<  Variables  >----------
-    // const user = useSelector(store => store.user)
-    
+    const user = useSelector(store => store.user)
+
 
 
     //---------------<  C l i c k   H a n d l e r s  >----------------------------
@@ -33,33 +35,45 @@ function ClassDetailsPage(){
         console.log('Selected class is:', classDetails.id);
 
         dispatch({
-            type:'ADD_RESERVATION', 
-            payload: classDetails 
+            type: 'ADD_RESERVATION',
+            payload: classDetails
         });
         alert("About to Add!")
         history.push('/my-classes')
     }
-    const handleGpsClick = () => {
-        console.log('This would ideally open google maps');
-    }
-    //---------------<  E N D  C l i c k   H a n d l e r s  >----------------------------
-    
-    
-    
-//---------------<  Temporary things to be deleted  >----------------------------
-// ----- DELETE this when we have a reducer/saga to replace it ------
-// const event = {id: 1,classname: "HIIT",description: "high intensity interval training",trainer_user_id: 2,date: "2022-04-12",start_time: "12:00:00",end_time: "13:00:00",location: "at the park?",class_size: 20};
-// ^^^^^^^^^^^^ DELETE this when we have a reducer/saga to replace it ^^^^^^^^^^^^^^^^^
 
-// To be completed on this page:
-// Eventually we'll get a trainer name, that will replace the trainer_user_id below
-// Link the gps to an actual google search query.
-//---------------< // END Temporary things to be deleted  >----------------------------
-console.log('these are the details pulled in from the reducer:', classDetails);
-console.log('this is the id pull from the url with params', id);
-    return(
+    function checkMatch() { // checks to see if class in details page matches a class the user is already signed up for
+        //-------------------- function isn't called on page load but with gps button click.
+        for (const myClass of classes) {
+            if (classDetails.id === myClass.id) {
+                setMatch(true);
+            }
+        }
+    }
+    const handleGpsClick = () => {
+        // console.log('This would ideally open google maps');
+        checkMatch();
+
+    }
+
+    //---------------<  E N D  C l i c k   H a n d l e r s  >----------------------------
+
+
+
+    //---------------<  Temporary things to be deleted  >----------------------------
+    // ----- DELETE this when we have a reducer/saga to replace it ------
+    // const event = {id: 1,classname: "HIIT",description: "high intensity interval training",trainer_user_id: 2,date: "2022-04-12",start_time: "12:00:00",end_time: "13:00:00",location: "at the park?",class_size: 20};
+    // ^^^^^^^^^^^^ DELETE this when we have a reducer/saga to replace it ^^^^^^^^^^^^^^^^^
+
+    // To be completed on this page:
+    // Eventually we'll get a trainer name, that will replace the trainer_user_id below
+    // Link the gps to an actual google search query.
+    //---------------< // END Temporary things to be deleted  >----------------------------
+    console.log('these are the details pulled in from the reducer:', classDetails);
+    console.log('this is the id pull from the url with params', id);
+    return (
         <>
-            <Nav/>
+            <Nav />
             <h1>{classDetails.date}</h1>
             <h3>{classDetails.classname}</h3>
             <h3>led by: {classDetails.trainer_user_id}</h3>
@@ -67,7 +81,7 @@ console.log('this is the id pull from the url with params', id);
             <h3>{classDetails.start_time}-{classDetails.end_time}</h3>
             <h3>{classDetails.description}</h3>
             <button onClick={() => handleReturnClick(classDetails)}>Return</button>
-            <button onClick={handleReserveClick}>Reserve</button>
+            <button onClick={handleReserveClick} disabled={match}>Reserve</button>
         </>
     )
 }
