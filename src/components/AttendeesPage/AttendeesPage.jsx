e018db70951f04c32b4b11e85190ee129029c657
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-04-18 16:23:12 -0500

    committing so I can update vs code. This might not be necessary but i want to anyways

--- src/components/AttendeesPage/AttendeesPage.jsx
+++ src/components/AttendeesPage/AttendeesPage.jsx
@@ -11,10 +11,19 @@ import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 function AttendeesPage(){
+    
+    //------------<  Setup  >-------------
     const dispatch = useDispatch();
     const attendees = useSelector(store => store.attendees);
-    const userId = useSelector(store => store.user.id);
+    // const userId = useSelector(store => store.user.id);
     const { id } = useParams()
+
+    // useEffect(() => {
+    //     dispatch({
+    //         type: 'FETCH_ATTENDANCE',
+    //         payload: id
+    //     });
+    // }, [])
 
     return(
         <>
