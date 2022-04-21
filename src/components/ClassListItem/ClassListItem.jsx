49861920765732c0ec55807c8fad7c77cc36bd89
Clay Webb <clay_webb@outlook.com>
2022-04-21 14:46:48 -0500

    Commented out extractFirstName to prevent current split method bug

--- src/components/ClassListItem/ClassListItem.jsx
+++ src/components/ClassListItem/ClassListItem.jsx
@@ -15,8 +15,9 @@ function ClassListItem({classEvent, i}) {
 
 
   const extractFirstName = (fullName) => { // This function will extract the first name of a trainer from their full name
-    let firstName = fullName.split(' ')[0]; // If the database changes from one name to a first name and last name, this will need to be changed as well
-    return firstName;
+    // let firstName = fullName.split(' ')[0]; // If the database changes from one name to a first name and last name, this will need to be changed as well
+    // return firstName;
+    return fullName;
   }
 
   return (
