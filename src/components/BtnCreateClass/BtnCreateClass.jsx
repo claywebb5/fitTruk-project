c0866401db0edf8f0de830ddbfed0255679cf35f
Clay Webb <clay_webb@outlook.com>
2022-04-21 12:01:00 -0500

    Admin create a class button now stays in the bottom right corner

--- src/components/BtnCreateClass/BtnCreateClass.jsx
+++ src/components/BtnCreateClass/BtnCreateClass.jsx
@@ -22,7 +22,7 @@ function BtnCreateClass() {
             <Box sx={{ height: 320, flexGrow: 1 }}>
                 <SpeedDial
                     ariaLabel="SpeedDial basic example"
-                    sx={{ position: 'absolute', bottom: 16, right: 16 }}
+                    sx={{ position: 'fixed', bottom: 16, right: 16 }}
                     icon={<SpeedDialIcon onClick={handleCreateClass} />}
                 >
                 </SpeedDial>
--- src/components/Nav/AdminNav.jsx
+++ src/components/Nav/AdminNav.jsx
@@ -9,10 +9,14 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+// ----------< MUI MENU >----------------
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import Avatar from '@mui/material/Avatar';
+// ----------< MUI DRAWER >----------------
+// import Drawer from '@mui/material/Drawer';
+
 
 
 // =================**< ADMIN VIEW >**=========================
