3b2d59f23b46ca9ba4127ca74bfe33f5813f4ac5
Clay Webb <clay_webb@outlook.com>
2022-04-24 16:11:06 -0500

    Added mui header to user page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -23,7 +23,17 @@ import EditIcon from '@mui/icons-material/Edit';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
-  // ======*** LINK TO MUI CHIPS FOR EDITABLE INPUTS: https://mui.com/material-ui/react-chip/
+
+const useStyles = makeStyles({
+    newroot: {
+        padding: 8,
+        '&:last-child': {
+            paddingBottom: 8,
+        },
+    },
+});
+
+// ======*** LINK TO MUI CHIPS FOR EDITABLE INPUTS: https://mui.com/material-ui/react-chip/
 
 
 
@@ -31,6 +41,8 @@ function UserPage() {
   //==================< SETUP >==========================
   const dispatch = useDispatch();
   const history = useHistory();
+  const classes = useStyles(); // MUI Theme
+
 
   //==================< VARIABLES >==========================
   const user = useSelector((store) => store.user);
@@ -56,28 +68,28 @@ function UserPage() {
   const [editUser, setEditUser] = useState(userObj);
   let initials = '';
 
-  //  ============< Pronoun Change >=============
+  //  ============< PRONOUN Change >=============
   const handlePronounChange = (event) => {
     setEditUser({ ...editUser, pronouns: event.target.value });
   };
-  //  ============< Address Change >=============
+  //  ============< STREET Change >=============
   const handleChangeStreet = (event) => {
     setEditUser({ ...editUser, street: event.target.value });
   };
-  //  ============< Pronoun Change >=============
+  //  ============< CITY Change >=============
   const handleChangeCity = (event) => {
     setEditUser({ ...editUser, city: event.target.value });
   };
-
+  //  ============< STATE Change >=============
   const handleChangeState = (event) => {
     setEditUser({ ...editUser, state: event.target.value });
   };
-
+  //  ============< ZIP Change >=============
   const handleChangeZip = (event) => {
     setEditUser({ ...editUser, zip: event.target.value });
   };
 
-  //  ============< Submit >==============================================
+  //  ============< SUBMIT >==============================================
   const handleSubmit = (event) => {
     event.preventDefault();
     let updatedUser = editUser;
@@ -89,16 +101,15 @@ function UserPage() {
       payload: updatedUser
     });
   }
-
+  //  ============< GO BACK >=============
   const handleReturnClick = () => {
     history.goBack();
     console.log('Clicked Cancel');
   }
-
+  //  ============< GET INITIALS >=============
   const getInitials = (nameObject) => {
     let firstLetter = 'H';
     let secondLetter = 'i';
-
     if (nameObject.first_name && nameObject.last_name) {
       firstLetter = (nameObject.first_name[0]).toUpperCase();
       secondLetter = (nameObject.last_name[0]).toUpperCase();
@@ -111,85 +122,111 @@ function UserPage() {
   }
 
 
+
+
+
   return (
     <>
-      <h1><u>Personal Info Page</u></h1>
+      <Container sx={{ border: 4, borderColor: '#c3c4c5', bgcolor: '#FFFFFF' }}>
 
-      {/* <button onClick={handleTest}>Test userObj</button> */}
-
-
-      <div className="container">
-        <form onSubmit={handleSubmit}>
-
-          <div> {/* CAN EDIT  */}
-            
-
-            {/* ------ This will conditionally render a two letter string from the first/last name of the user, and it won't break the app if either of those two values isn't present ------ */}
-            {(getInitials(user)) && <Avatar sx={{ bgcolor: '#ace23a' }}>{initials}</Avatar>}
-
-            {/*=====< AVATAR WITH USER PROFILE PICTURE >====*/}
-            {/* <Avatar src={user.profile_image} /> */}
-
-          </div>
-
-          <h2>Welcome, {user.first_name}!</h2>
-          <div> {/* CAN EDIT  */}
-            <p><b>Pronouns:</b> {user.pronouns}</p>
-            <select onChange={handlePronounChange} value={editUser.pronouns}>
-              <option value="He/Him"> He/Him</option>
-              <option value="She/Her"> She/Her</option>
-              <option value="They/Them"> They/Them</option>
-            </select>
-          </div>
-          <p><b>Email:</b> {user.email}</p>
-          <p><b>Number:</b> {user.phone_number}</p>
-
-          <div> {/* CAN EDIT  */}
-            <p><b>Street:</b> {user.street}</p>
-            <input
-              type="text"
-              placeholder={user.street}
-              value={editUser.street}
-              onChange={handleChangeStreet}
-            />
-          </div>
-          <div>
-            <p><b>City:</b> {user.city}</p>
-            <input
-              type="text"
-              placeholder={user.city}
-              value={editUser.city}
-              onChange={handleChangeCity}
-            />
-          </div>
-          <div>
-            <p><b>State:</b> {user.state}</p>
-            <input
-              type="text"
-              placeholder={user.state}
-              value={editUser.state}
-              onChange={handleChangeState}
-            />
-          </div>
-          <div>
-            <p><b>Zip:</b> {user.zip}</p>
-            <input
-              type="text"
-              placeholder={user.zip}
-              value={editUser.zip}
-              onChange={handleChangeZip}
-            />
-          </div>
-
-          <p>------- In case of emergencies ---------</p>
-          <p><b>Emergency Contact:</b> {user.emergency_name}</p>
-          <p><b>Number:</b> {user.emergency_number}</p>
-          <button onClick={handleReturnClick}>Cancel</button>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+        {/* ============< HEADER >============== */}
+        <Card sx={{ bgcolor: '#6d6e71', color: '#FFFFFF' }}>
+          <CardContent className={classes.newroot}>
+            {/* <Box sx={{ display: 'flex', flexDirection: 'row' }}> */}
+              <Grid container justifyContent="center" alignItems="center" direction="row" spacing={2}>
+                <Grid item>
+                  <Typography variant="h5">
+                    Welcome, {user.first_name}!
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Avatar src={user.profile_image} sx={{ height: '70px', width: '70px' }} />
+                </Grid>
+              </Grid>
+            {/* </Box> */}
+          </CardContent>
+        </Card>
 
 
+        <Typography variant="h5" align="center">
+
+        </Typography>
+
+
+
+
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+
+            <div> {/* CAN EDIT  */}
+
+
+              {/* ------ This will conditionally render a two letter string from the first/last name of the user, and it won't break the app if either of those two values isn't present ------ */}
+              {(getInitials(user)) && <Avatar sx={{ bgcolor: '#ace23a' }}>{initials}</Avatar>}
+
+              {/*=====< AVATAR WITH USER PROFILE PICTURE >====*/}
+              {/* <Avatar src={user.profile_image} /> */}
+
+            </div>
+
+            <h2></h2>
+            <div> {/* CAN EDIT  */}
+              <p><b>Pronouns:</b> {user.pronouns}</p>
+              <select onChange={handlePronounChange} value={editUser.pronouns}>
+                <option value="He/Him"> He/Him</option>
+                <option value="She/Her"> She/Her</option>
+                <option value="They/Them"> They/Them</option>
+              </select>
+            </div>
+            <p><b>Email:</b> {user.email}</p>
+            <p><b>Number:</b> {user.phone_number}</p>
+
+            <div> {/* CAN EDIT  */}
+              <p><b>Street:</b> {user.street}</p>
+              <input
+                type="text"
+                placeholder={user.street}
+                value={editUser.street}
+                onChange={handleChangeStreet}
+              />
+            </div>
+            <div>
+              <p><b>City:</b> {user.city}</p>
+              <input
+                type="text"
+                placeholder={user.city}
+                value={editUser.city}
+                onChange={handleChangeCity}
+              />
+            </div>
+            <div>
+              <p><b>State:</b> {user.state}</p>
+              <input
+                type="text"
+                placeholder={user.state}
+                value={editUser.state}
+                onChange={handleChangeState}
+              />
+            </div>
+            <div>
+              <p><b>Zip:</b> {user.zip}</p>
+              <input
+                type="text"
+                placeholder={user.zip}
+                value={editUser.zip}
+                onChange={handleChangeZip}
+              />
+            </div>
+
+            <p>------- In case of emergencies ---------</p>
+            <p><b>Emergency Contact:</b> {user.emergency_name}</p>
+            <p><b>Number:</b> {user.emergency_number}</p>
+            <button onClick={handleReturnClick}>Cancel</button>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+      </Container>
     </>
   );
 }
