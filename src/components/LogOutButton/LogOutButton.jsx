2b13270593e195a0c9402d26d1287a81c52e0ccd
Josh Kralewski <joshkmanj@gmail.com>
2022-04-25 19:46:06 -0500

    Landing page styling updated

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import Button from '@mui/material/Button';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function LogOutButton(props) {
   const dispatch = useDispatch();
+  // const history = useHistory();
+
+  const handleLogOut = () => {
+    dispatch({ type: 'LOGOUT' })
+    // history.push('/home')
+  }
+
   return (
     <button
       // This button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
       className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
+      onClick={handleLogOut}
     >
       Log Out
     </button>
