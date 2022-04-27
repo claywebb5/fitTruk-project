1e8c2ed6f5dc2035ffe0752bd6b3bb03e88ab6fc
colin jaworski <colinrjaworski.84@gmail.com>
2022-04-27 16:05:06 -0500

    removed reduxStore.js file

--- src/components/TrainerRoute/TrainerRoute.jsx
+++ src/components/TrainerRoute/TrainerRoute.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import LoginPage from '../LoginPage/LoginPage';
-import {useSelector} from 'react-redux';
-
-// A Custom Wrapper Component -- This will keep our code DRY.
-// Responsible for watching redux state, and returning an appropriate component
-// API for this component is the same as a regular route
-
-// THIS IS NOT SECURITY! That must be done on the server
-// A malicious user could change the code and see any view
-// so your server-side route must implement real security
-// by checking req.isAuthenticated for authentication
-// and by checking req.user for authorization
+import { useSelector } from 'react-redux';
 
 function TrainerRoute({ component, children, ...props }) {
   const user = useSelector((store) => store.user);
