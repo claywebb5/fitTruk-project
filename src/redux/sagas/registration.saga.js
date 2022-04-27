d0344db9d07e91aaef7bdf098027bb4562567078
colin jaworski <colinrjaworski.84@gmail.com>
2022-04-27 16:21:53 -0500

    finished removing unnessary things in SRC folder

--- src/redux/sagas/registration.saga.js
+++ src/redux/sagas/registration.saga.js
@@ -17,7 +17,6 @@ function* registerUser(action) {
     // after registration or after they log out
     yield put({ type: 'SET_TO_LOGIN_MODE' }); // * NO CLUE WHERE THIS GOES???
   } catch (error) {
-    console.log('Error with user registration:', error);
     yield put({ type: 'REGISTRATION_FAILED' }); // * Goes to REDUCER "errors.reducer"
   }
 }
--- src/redux/sagas/user.saga.js
+++ src/redux/sagas/user.saga.js
@@ -19,7 +19,6 @@ function* fetchUser() {
     // the client-side code know the user is logged in
     yield put({ type: 'SET_USER', payload: response.data }); // * Goes to REDUCER "user.reducer"
   } catch (error) {
-    console.log('User get request failed', error);
   }
 }
 
