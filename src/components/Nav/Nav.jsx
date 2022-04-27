fde9f1f8de244df0fcf395c0fb821f6a3735593b
colin jaworski <colinrjaworski.84@gmail.com>
2022-04-27 15:53:23 -0500

    removed logs and unused code from landingPage-Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,40 +1,12 @@
-// import './Nav.css';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import AdminNav from './AdminNav';
 import ProspectNav from './ProspectNav';
 import CustomerNav from './CustomerNav';
 
-
-
-// =================**< NAV BAR PARENT COMPONENT >**=========================
-
-// ===========*< COLOR/FONT THEME >*===============
-// const theme = createTheme({
-//   typography: {
-//     fontFamily: [
-//       'FATFRANK',
-//       'CENTURY GOTHIC',
-//       'Montserrat',
-//     ].join(','),
-//   },
-//   palette: {
-//     mode: 'main',
-//     primary: {
-//       darkGreen: '#80bd02',
-//       lightGreen: '#ace23a',
-//       darkGrey: '#41414c',
-//       lightGrey: '#6d6e71'
-//     },
-//   },
-// })
-
-
-
 function Nav() {
   // ========< TOOLS >==============
   const user = useSelector((store) => store.user);
-
 
   return (
     <>
