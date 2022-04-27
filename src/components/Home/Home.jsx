8551e066061f66bdb1980f193dd082aaa7a006b9
colin jaworski <colinrjaworski.84@gmail.com>
2022-04-27 16:29:12 -0500

    removed home folder

--- src/components/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-// =======================**< POSSIBLE LOGGED IN HOME PAGE >**==================================
-
-// Could include the following:
-    // - Full FitTruk video
-    // - "What is FitTruk?" information (Mission statement/values)
-    // - "Want to host an event?" information
-    
-// ** See https://fittrukkc.com/ for examples **
